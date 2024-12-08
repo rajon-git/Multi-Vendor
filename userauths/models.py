5e0c4ee47cb879607b6bfefb8ca93e8017e0aa31
rajon-git <rajon.zhsust15@gmail.com>
2024-12-08 21:05:40 +0600

    abstract user model

--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        email_username, mobile = self.email.split("@")
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = email_username
+
+        if self.username == "" or self.full_name == None:
+            self.username = email_username
+
+        super(User, self).save(*args, **kwargs)

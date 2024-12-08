b26726a41c1a52782ad881326423f9c3d1dd4c34
rajon-git <rajon.zhsust15@gmail.com>
2024-12-08 21:28:29 +0600

    user profile model create

--- userauths/models.py
+++ userauths/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from shortuuid.django_fields import ShortUUIDField
 
 class User(AbstractUser):
     username = models.CharField(max_length=100)
@@ -22,3 +23,30 @@ class User(AbstractUser):
             self.username = email_username
 
         super(User, self).save(*args, **kwargs)
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    gender = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefghijk")
+
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+        
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = self.user.full_name
+
+        super(Profile, self).save(*args, **kwargs)
+

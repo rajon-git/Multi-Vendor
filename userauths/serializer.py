f1123c45cab54bb58e05740bcbe5ec84c79f4726
rajon-git <rajon.zhsust15@gmail.com>
2024-12-09 20:56:01 +0600

    token serializer created

--- userauths/serializer.py
+++ userauths/serializer.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User, Profile
 
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['full_name'] = user.full_name
+        token['username'] = user.username
+        token['email'] = user.email
+        try:
+            token['vendor_id'] = user.vendor.id
+        except:
+            token['vendor_id'] = 0
+
+        return token 
+    
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User

c7e44e43ef5a5fffe88091491ad309afa43c5489
rajon-git <rajon.zhsust15@gmail.com>
2024-12-11 15:55:30 +0600

    refresh token url create

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from userauths import views as userauth_views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('user/token/', userauth_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauth_views.RegisterView.as_view())
 ]

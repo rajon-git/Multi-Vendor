038b8aff433bb74300894a83eb5373c985219395
rajon-git <rajon.zhsust15@gmail.com>
2024-12-21 20:51:43 +0600

    password reset api url create

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('user/token/', userauth_views.MyTokenObtainPairView.as_view()),
     path('user/token/refresh/', TokenRefreshView.as_view()),
-    path('user/register/', userauth_views.RegisterView.as_view())
+    path('user/register/', userauth_views.RegisterView.as_view()),
+    path('user/password-reset/<email>/', userauth_views.PasswordResetEmailVerify.as_view())
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
 from userauths.models import User, Profile
-from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+import random
+import shortuuid
 
 # Create your views here.
 
@@ -14,3 +16,29 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny, )
     serializer_class = RegisterSerializer
+
+def generate_otp():
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:6]
+    return unique_key
+
+class PasswordResetEmailVerify(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects.get(email=email)
+
+        if user:
+            user.otp = generate_otp()
+            user.save()
+
+            uidb64 = user.pk
+            otp = user.otp
+
+            link = f"http://localhost:5173/create-new-password?otp={otp}&uidb64={uidb64}"
+            print('link ====', link)
+
+            # send email
+        return user

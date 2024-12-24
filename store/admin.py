ba56be5d2016135968810072f57db3d44e1032ee
rajon-git <rajon.zhsust15@gmail.com>
2024-12-24 21:49:27 +0600

    product specification , color, size ,gallery added

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Gallery, Specification, Size, Color
 
 # Register your models here.
+class GalleryInline(admin.TabularInline):
+    model = Gallery
+    extra = 0
+
+class SpecificationInline(admin.TabularInline):
+    model = Specification
+    extra = 0
+
+class SizeInline(admin.TabularInline):
+    model = Size
+    extra = 0
+
+class ColorInline(admin.TabularInline):
+    model = Color
+    extra = 0
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'shipping_amount', 'stock_qty', 'in_stock', 'vendor','featured']
+    list_editable = ['featured']
+    list_filter = ['date']
+    search_fields = ['title']
+    inlines = [GalleryInline, SpecificationInline, SizeInline, ColorInline]
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -54,3 +54,38 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Gallery(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="product", default="product.jpg", null=True, blank=True)
+    active = models.BooleanField(default=True)
+    gid = ShortUUIDField(unique=True, length = 10, alphabet="abcdefg12345") 
+
+    def __str__(self):
+        return self.product.title
+    class Meta:
+        verbose_name_plural = "Product Images"
+    
+class Specification(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000)
+    content = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+    
+class Size(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.name
+    
+class Color(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000)
+    color_code = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name

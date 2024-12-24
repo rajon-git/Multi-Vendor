5d2887023f85a12c6d9cf58b56e2bf5e4e292556
rajon-git <rajon.zhsust15@gmail.com>
2024-12-24 21:18:57 +0600

    product category and product model create

--- store/models.py
+++ store/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 from userauths.models import User, Profile
+from vendor.models import Vendor
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.text import slugify
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="category", default="category.jpg", null=True, blank=True)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    STATUS = (
+        ("draft", "Draft"),
+        ("disabled", "Disabled"),
+        ("in_review", "In Review"),
+        ("published", "Published"),
+    )
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="product", default="product.jpg", null=True, blank=True)
+    description = models.TextField(blank=True,null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    shipping_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    stock_qty = models.PositiveIntegerField(default=1)
+    in_stock = models.BooleanField(default=True)
+
+    status = models.CharField(max_length=100, choices=STATUS, default="published")
+    featured = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=0)
+    ratings = models.PositiveIntegerField(default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    pid = ShortUUIDField(unique=True, length = 10, alphabet="abcdefg12345") 
+    slug= models.SlugField(unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+
+        super(Vendor, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title

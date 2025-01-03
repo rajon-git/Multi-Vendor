a7ce6929f5412d279598dc12e2dc05d8b53ff973
rajon-git <rajon.zhsust15@gmail.com>
2025-01-03 22:55:19 +0600

    cart model create

--- store/models.py
+++ store/models.py
@@ -89,3 +89,22 @@ class Color(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    sub_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    shipping_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    service_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    tax_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    country = models.CharField(max_digits=100, blank=True, null=True)
+    size = models.CharField(max_digits=100, blank=True, null=True)
+    color = models.CharField(max_digits=100, blank=True, null=True)
+    cart_id = models.CharField(max_digits=1000, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.cart_id} - {self.product.title}"

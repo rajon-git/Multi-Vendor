39fd03086d2ea02cf72b570c8292a5bb1a465c7f
rajon-git <rajon.zhsust15@gmail.com>
2025-01-06 22:07:52 +0600

    category views and url create

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from userauths import views as userauth_views
+from store import views as store_views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -7,5 +8,8 @@ urlpatterns = [
     path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauth_views.RegisterView.as_view()),
     path('user/password-reset/<email>/', userauth_views.PasswordResetEmailVerify.as_view()),
-    path('user/password-change/', userauth_views.PasswordChangeView.as_view())
+    path('user/password-change/', userauth_views.PasswordChangeView.as_view()),
+
+    #store endpoints
+    path('category/', store_views.CategoryListAPIView.as_view()),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
+from .serializer import CategorySerializer, ProductFaqSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 # Create your views here.
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]

fe13ea0886932300b9c379b517b9e8df63dbfff4
rajon-git <rajon.zhsust15@gmail.com>
2025-01-06 21:20:48 +0600

    product serializer create

--- store/serializer.py
+++ store/serializer.py
@@ -6,3 +6,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'

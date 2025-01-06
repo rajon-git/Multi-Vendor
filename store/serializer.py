96a9fe7eb21fabb2299d38cf933a867c4231bd5c
rajon-git <rajon.zhsust15@gmail.com>
2025-01-06 21:50:40 +0600

    cart serializer create

--- store/serializer.py
+++ store/serializer.py
@@ -68,3 +68,17 @@ class ProductSerializer(serializers.ModelSerializer):
             self.Meta.depth = 0
         else:
             self.Meta.depth = 3
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CartSerializer, self).__init__(*args, **kwargs)
+
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 3
